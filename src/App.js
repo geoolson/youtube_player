974d842434f866a28800b5eed98c6bd1638899d3
Geoffrey <geoolson@pdx.edu>
2020-08-25 21:57:44 -0700

    some styling changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React, { useState } from 'react';
+import './app.css';
 
 const App = () => {
   const [ url, setUrl ] = useState("https://www.youtube.com/embed/tgbNymZ7vqY");
+  const width = document.body.clientWidth < 420 ? document.body.clientWidth : 420;
+  const height = width * (9/16);
   return (
     <div>
       <form
         onSubmit={e => {
           e.preventDefault();
           const link = e.target.link.value;
-          if(link.includes(`/embed/`))
+          if(link.includes('/embed/'))
             setUrl(link);
-          else if(link.includes(`youtu.be/`))
-            setUrl(`https://youtube.com/embed/${link.split(`youtu.be/`)[1]}`);
+          else if(link.includes('youtu.be/'))
+            setUrl(`https://youtube.com/embed/${link.split('youtu.be/')[1]}`);
           else
             setUrl(`https://youtube.com/embed/${link.split("?v=")[1]}`);
         }}
       >
-        <input 
-          type="text"
-          name="link"
-        />
+        <input type="text" name="link" />
         <button>Submit</button>
       </form>
       <iframe
         frameBorder={0}
-        width={ document.body.clientWidth < 420 ? document.body.clientWidth : 420} 
+        width={width}
+        height={height}
         src={url}
       />
     </div>
